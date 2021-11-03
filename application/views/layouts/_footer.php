9c67a1a265b1608af26dbe4eba47b7db5b1ffdd0
Roman Batavi <romanbatavi98@gmail.com>
2021-11-03 23:56:22 +0700

    first init

---
+++ application/views/layouts/_footer.php
@@ -0,0 +1,10 @@
+<footer class="footer mt-5">
+	<nav class="navbar navbar-dark bg-light sticky-bottom">
+		<div class="container d-flex justify-content-center">
+			<div class="copyright text-info p-3">
+				<small class="text-info p-3">Copyright &copy; G.STORE 2020 | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+				</small>
+			</div>
+		</div>
+	</nav>	
+</footer>  
